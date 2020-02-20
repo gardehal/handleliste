366a8d476a2e0a47ff56c1f902678e13b1429324
grdall <alex.thogar@gmail.com>
2020-02-20 17:00:50 +0100

    Update connection.php

--- require/connection.php
+++ require/connection.php
@@ -3,7 +3,7 @@
 //OBS: ikke bruk personlig passord, lag nye
 
 $host = 'localhost'; //Host, localhost for MAMP, tek.westerdals.no for server
-$port = 8889; //3306 for cPanel, 8889 i localhost(eller hva du har i MAMP)
+$port = mysql; //3306 for cPanel, 8889 i localhost(eller hva du har i MAMP)
 $username = 'root'; //Brukernavn i cPanel 
 $password = 'root'; //Passord for bruker i cPanel   
 $name = 'handleliste';
